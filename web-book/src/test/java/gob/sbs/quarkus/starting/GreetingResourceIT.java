621daa347a74d7e0b2235a5c2d833a5b7c6821d2
José Díaz <jamdiazdiaz@gmail.com>
2022-11-03 17:10:31 -0500

    First Draft

---
+++ web-book/src/test/java/gob/sbs/quarkus/starting/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package gob.sbs.quarkus.starting;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
