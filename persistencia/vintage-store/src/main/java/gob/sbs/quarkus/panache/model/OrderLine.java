e4eea81127882515c293eb4c2c8312497a480fa0
José Díaz <jamdiazdiaz@gmail.com>
2022-11-08 21:26:21 -0500

    otros modulos

---
+++ persistencia/vintage-store/src/main/java/gob/sbs/quarkus/panache/model/OrderLine.java
@@ -0,0 +1,34 @@
+package gob.sbs.quarkus.panache.model;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.json.bind.annotation.JsonbTransient;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.Instant;
+
+
+@Entity
+@Table(name = "t_purchase_order_lines")
+public class OrderLine extends PanacheEntity {
+
+  @ManyToOne(cascade = CascadeType.PERSIST)
+  @JoinColumn(name = "item_fk")
+  public Item item;
+
+  @Column(nullable = false)
+  public Integer quantity;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "purchase_order_fk")
+  @JsonbTransient
+  public PurchaseOrder purchaseOrder;
+
+  @Column(name = "created_date", nullable = false)
+  public Instant createdDate = Instant.now();
+
+}
--- persistencia/vintage-store/src/main/java/gob/sbs/quarkus/panache/model/Publisher.java
+++ persistencia/vintage-store/src/main/java/gob/sbs/quarkus/panache/model/Publisher.java
@@ -2,6 +2,8 @@ package gob.sbs.quarkus.panache.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -23,11 +25,25 @@ public class Publisher extends PanacheEntity {
   public Publisher() {
   }
 
+
+  // ======================================
+  // =              Methods               =
+  // ======================================
+
+  public static List<Publisher> findContainingName(String name) {
+    return Publisher.list("name like ?1", "%" + name + "%");
+  }
+
+  public static Optional<Publisher> findByName(String name) {
+    return Publisher.find("name", name).firstResultOptional();
+  }
+
   @Override
   public String toString() {
     return "Publisher{" +
         "name='" + name + '\'' +
         ", createdDate=" + createdDate +
+        ", id=" + id +
         '}';
   }
 }
