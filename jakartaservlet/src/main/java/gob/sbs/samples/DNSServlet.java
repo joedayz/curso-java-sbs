34503447e8a2593632cc5f05fec5b1a9e89f68d8
José Díaz <jamdiazdiaz@gmail.com>
2022-11-10 19:48:50 -0500

    doGet en DNSServlet

--- jakartaservlet/src/main/java/gob/sbs/samples/DNSServlet.java
+++ jakartaservlet/src/main/java/gob/sbs/samples/DNSServlet.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -16,6 +18,22 @@ public class DNSServlet extends HttpServlet {
 
   public void init() throws ServletException{
     ipMap = new ConcurrentHashMap<>();
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    String domainName = req.getParameter("domainName");
+
+    if(domainName==null){
+      resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+      return;
+    }
+
+    PrintWriter printWriter = resp.getWriter();
+    printWriter.println(Optional.ofNullable(ipMap.get(domainName)).orElse(""));
+
   }
 
   @Override
