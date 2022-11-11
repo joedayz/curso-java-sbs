7ca621bd89a2cef47cf69aebe8d720a0d993f257
José Díaz <jamdiazdiaz@gmail.com>
2022-11-10 20:34:55 -0500

    usando filter

---
+++ jakartaservlet/src/main/java/gob/sbs/samples/AuthorizationFilter.java
@@ -0,0 +1,50 @@
+package gob.sbs.samples;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@WebFilter("/security/*")
+public class AuthorizationFilter implements Filter {
+
+  private Map<String, String> passwords;
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+    passwords = new ConcurrentHashMap<>();
+    passwords.put("admin", "admin");
+    passwords.put("developer", "dev123");
+  }
+
+  @Override
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+      FilterChain filterChain) throws IOException, ServletException {
+
+    if(!validateAuth((HttpServletRequest) servletRequest, (HttpServletResponse) servletResponse)){
+      ((HttpServletResponse) servletResponse).sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized request");
+    }else{
+      filterChain.doFilter(servletRequest, servletResponse);
+    }
+  }
+
+  private boolean validateAuth(HttpServletRequest request, HttpServletResponse servletResponse) {
+    String login = request.getParameter("login");
+    if(login==null || !passwords.containsKey(login)){
+      return false;
+    }
+    String password = request.getParameter("password");
+    if(password==null || !passwords.get(login).equals(password)){
+      return false;
+    }
+    return true;
+  }
+}
---
+++ jakartaservlet/src/main/java/gob/sbs/samples/HomeServlet.java
@@ -0,0 +1,23 @@
+package gob.sbs.samples;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/security")
+public class HomeServlet extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    resp.setContentType("text/html");
+    PrintWriter out = resp.getWriter();
+
+    out.print("Welcome to SBS");
+    out.close();
+  }
+}
