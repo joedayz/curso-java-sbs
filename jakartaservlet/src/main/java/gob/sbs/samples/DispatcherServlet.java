bd994269d73d844c868caf4ce99e056a8df7ddfe
José Díaz <jamdiazdiaz@gmail.com>
2022-11-10 20:49:14 -0500

    request dispather , forward e include

---
+++ jakartaservlet/src/main/java/gob/sbs/samples/DispatcherServlet.java
@@ -0,0 +1,36 @@
+package gob.sbs.samples;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(value="/helloworld.html")
+public class DispatcherServlet extends HttpServlet {
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    String type = req.getParameter("type");
+
+    PrintWriter writer = resp.getWriter();
+    writer.println("<html>");
+    writer.println("<head>");
+    writer.println("</head>");
+    writer.println("<body>");
+
+    var dispatcher= req.getRequestDispatcher("/helloworld");
+
+    if("forward".equals(type)){
+      dispatcher.forward(req, resp);
+    }else{
+      dispatcher.include(req, resp);
+    }
+
+    writer.println("</body>");
+    writer.println("</html>");
+  }
+}
--- jakartaservlet/src/main/java/gob/sbs/samples/HelloWorldServlet.java
+++ jakartaservlet/src/main/java/gob/sbs/samples/HelloWorldServlet.java
@@ -7,13 +7,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name="HelloWorld", value="/hello")
+@WebServlet(name="HelloWorld", value="/helloworld")
 public class HelloWorldServlet extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    resp.getWriter().println("Hello World");
+    resp.getWriter().println("Hello SBS");
   }
 
 
