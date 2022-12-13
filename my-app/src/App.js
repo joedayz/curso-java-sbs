d646324b0d54dfdaab716d7d5dfc29abf35eadf6
José Díaz <jamdiazdiaz@gmail.com>
2022-12-13 18:22:48 -0500

    salon api

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+
+class App extends Component{
+  render(){
+    return (
+      <>
+        <h1>Hola SBS</h1>
+        <h2>From Component React</h2>
+      </>
+    );
+  }
+}
+
+export default App;
